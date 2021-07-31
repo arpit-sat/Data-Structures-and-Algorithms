2adb84f8212c5ad2480f547b6bf17aae61b5f59f
Arpit Satnalika <60641110+arpit-sat@users.noreply.github.com>
2021-07-31 12:14:47 +0530

    Time: 116 ms (64.09%), Space: 9.9 MB (96.54%) - LeetHub

--- maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
+++ maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    int get_val(vector<int> &student,vector<int> &mentor)
+    int get_val(vector<int> &a, vector<int> &b)
     {
-        int val=0;
-        for(int i=0;i<student.size();i++)
+        int ans =0 ;
+        for(int i=0 ; i<a.size(); i++)
+            ans += (a[i] == b[i]) ? 1 : 0;
+        return ans;
+    }
+    
+    int solve(vector<vector<int>> &student, vector<vector<int>> &mentor, vector<bool> &visited, int index)
+    {
+        int n = mentor.size();
+        if(index == n)
+            return 0;
+        int ans = 0;
+        for(int i=0 ; i<n ; i++)
         {
-            if(student[i]==mentor[i])
+            if(visited[i] == 0)
             {
-                val++;
+                visited[i] = 1;
+                int currAns = solve(student , mentor , visited , index+1) + get_val(student[index] , mentor[i]);
+                visited[i] = 0;
+                ans = max(ans , currAns);
             }
         }
-        return val;
+        return ans;
     }
-void find_max(vector<vector<int>>&students, vector<vector<int>>& mentor,
-int &max_score,int curr_score,int j,vector<bool> &visited)
-{
-	if(j==students.size())
-	{
-		max_score=max(max_score,curr_score);
-	}
-	for(int i=0;i<students.size();i++)
-	{
-		if(visited[i]==false)
-		{
-			visited[i]=true;
-			int temp=get_val(students[i],mentor[j]);
-			find_max(students,mentor,max_score,curr_score+temp,j+1,visited);
-			visited[i]=false;
-		}
-	}
-}
-
-    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) 
+    
+    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentor) 
     {
-        int max_score=0;
-        int n=students.size();
-        vector<bool> visited(n);
-        find_max(students,mentors,max_score,0,0,visited);
-        return max_score;
+        vector<bool> visited(mentor.size());
+        int ans = solve(students , mentor , visited , 0);
+        return ans;
     }
+
 };
